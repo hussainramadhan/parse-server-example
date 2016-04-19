14d5b89a72f8e760750b0cedccf6f77793298ea1
hussainramadhan <ramadhan.hussain@gmail.com>
2016-04-19 21:50:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ if (!databaseUri) {
 }
 
 var api = new ParseServer({
-  serverURL: "https://your-app-name.herokuapp.com/parse",
+  serverURL: "http://localhost:1337",
   databaseURI: databaseUri || 'mongodb://heroku_9v5rjfbg:jll2vbbrcju211r320gb2n16cu@ds011331.mlab.com:11331/heroku_9v5rjfbg',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
